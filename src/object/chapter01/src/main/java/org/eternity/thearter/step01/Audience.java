57cda4d3aae241fa2b0b41f74d8c33fd286fd307
YoonSung Kim <opqudk@gmail.com>
2020-04-28 10:12:09 +0900

    Audience

---
+++ src/object/chapter01/src/main/java/org/eternity/thearter/step01/Audience.java
@@ -0,0 +1,13 @@
+package object.chapter01.src.main.java.org.eternity.thearter.step01;
+
+public class Audience {
+    private Bag bag;
+
+    public Audience(Bag bag) {
+        this.bag = bag;
+    }
+
+    public Bag getBag() {
+        return bag;
+    }
+}
