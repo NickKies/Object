a80a436c8b75f9f1ad5f90ee331c65913624b374
YoonSung Kim <opqudk@gmail.com>
2020-05-09 15:45:52 +0900

    step03

---
+++ src/object/chapter01/src/main/java/org/eternity/thearter/step03/Audience.java
@@ -0,0 +1,13 @@
+package object.chapter01.src.main.java.org.eternity.thearter.step03;
+
+public class Audience {
+    private Bag bag;
+
+    public Audience(Bag bag) {
+        this.bag = bag;
+    }
+
+    public Long buy(Ticket ticket) {
+        return bag.hold(ticket);
+    }
+}
