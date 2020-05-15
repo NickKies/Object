9405877897e42bf67c20437b651924f835dfefb4
YoonSung Kim <opqudk@gmail.com>
2020-05-15 14:54:03 +0900

    movie

---
+++ src/object/chapter02/src/main/java/org/eternity/movie/step01/pricing/AmountDiscountPolicy.java
@@ -0,0 +1,20 @@
+package object.chapter02.src.main.java.org.eternity.movie.step01.pricing;
+
+import object.chapter02.src.main.java.org.eternity.money.Money;
+import object.chapter02.src.main.java.org.eternity.movie.step01.DiscountCondition;
+import object.chapter02.src.main.java.org.eternity.movie.step01.DiscountPolicy;
+import object.chapter02.src.main.java.org.eternity.movie.step01.Screening;
+
+public class AmountDiscountPolicy extends DiscountPolicy {
+    private Money discountAmount;
+
+    public AmountDiscountPolicy(Money discountAmount, DiscountCondition... conditions) {
+        super(conditions);
+        this.discountAmount = discountAmount;
+    }
+
+    @Override
+    protected Money getDiscountAmount(Screening screening) {
+        return discountAmount;
+    }
+}
