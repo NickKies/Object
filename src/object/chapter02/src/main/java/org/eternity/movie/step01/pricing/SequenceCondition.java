56b4bed872b06bf3a127cb32d059d33a5dd14fbe
YoonSung Kim <opqudk@gmail.com>
2020-05-17 10:51:42 +0900

    movie

---
+++ src/object/chapter02/src/main/java/org/eternity/movie/step01/pricing/SequenceCondition.java
@@ -0,0 +1,17 @@
+package object.chapter02.src.main.java.org.eternity.movie.step01.pricing;
+
+import object.chapter02.src.main.java.org.eternity.movie.step01.DiscountCondition;
+import object.chapter02.src.main.java.org.eternity.movie.step01.Screening;
+
+public class SequenceCondition implements DiscountCondition {
+    private int sequence;
+
+    public SequenceCondition(int sequence) {
+        this.sequence = sequence;
+    }
+
+    public boolean isSatisfiedBy(Screening screening) {
+        return screening.isSequence(sequence);
+    }
+}
+
